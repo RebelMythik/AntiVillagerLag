b43e4ee827f04a536cade3cb5a4690c523f5f372
RebelMythik <fordjmf@hotmail.com>
2022-06-24 14:30:58 -0400

    More Work

--- src/main/java/rebelmythik/antivillagerlag/events/TradeRestocks.java
+++ src/main/java/rebelmythik/antivillagerlag/events/TradeRestocks.java
@@ -1,15 +1,25 @@
 package rebelmythik.antivillagerlag.events;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import rebelmythik.antivillagerlag.AntiVillagerLag;
+
+import java.util.List;
 
 public class TradeRestocks implements Listener {
 
     public AntiVillagerLag plugin;
+    public long restock1 = 1000;
+    public long restock2 = 13000;
+
 
     public TradeRestocks(AntiVillagerLag plugin) {
         this.plugin = plugin;
@@ -46,36 +56,49 @@ public class TradeRestocks implements Listener {
 
     @EventHandler
     public void clickVillager(PlayerInteractEntityEvent e) {
+        //is it the villager we want?
         if (!e.getRightClicked().getType().equals(EntityType.VILLAGER)) return;
         Villager vil = (Villager) e.getRightClicked();
-        if (!vil.getName().equals(plugin.getConfig().getString("NameThatDisables"))) return;
-
-        //Code if the villager is braindead to restock it
+        if (!vil.getCustomName().equals(plugin.getConfig().getString("NameThatDisables"))) return;
+        //If he doesn't have a time, restock
+        System.out.println(hasTime(vil));
+        if (!hasTime(vil)) {
+            setNewTime(vil);
+            System.out.println("Should have time now");
+        }
+        //if he does have a time, get it; also create time variables
         World world = plugin.getServer().getWorld("world");
         long time = world.getGameTime();
-        long lastRestock = 0;
-        long curDay = time / numOfTicksInDay;
+        long numOfTicksInDay = 24000;
         long curTick = time % numOfTicksInDay;
-        //Code if the day is over
-        /*
-        if gameTime > day of last restock {
-            if gameTime > firsttime
-                restock villager code here
+        long curDay = (time - curTick) / numOfTicksInDay;
+        long lastTime = getTime(vil);
+        long vilTick = lastTime % numOfTicksInDay;
+        long vilDay = (lastTime - vilTick) / numOfTicksInDay;
+
+        //Check if he should be restocked
+        System.out.println("lastTime: " + Long.toString(lastTime));
+        System.out.println("curDay: " + Long.toString(curDay));
+        System.out.println("curTick: " + Long.toString(curTick));
+        System.out.println("vilDay: " + Long.toString(vilDay));
+        System.out.println("vilTick: " + Long.toString(vilTick));
+        if (curDay == vilDay) {
+            if (curTick >= restock2) {
+                if (vilTick <= restock2) {
+                    restock(vil);
+                    setNewTime(vil);
+                }
+            } else if (curTick >= restock1) {
+                if (vilTick <= restock1) {
+                    restock(vil);
+                    setNewTime(vil);
+                }
+            }
+        } else {
+            if (curTick >= restock1) {
+                restock(vil);
+                setNewTime(vil);
+            }
         }
-
-        else
-
-        Check if the tick time is past on the same day
-        then restock
-        */
-        //Code if the day is same and check if its past the next tick time
-
-
-
-
-
     }
-
-
-
 }
