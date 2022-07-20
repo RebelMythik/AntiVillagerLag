0c04e960d1a91db7affc4dd4afead4701a670ce0
RebelMythik <50276986+RebelMythik@users.noreply.github.com>
2022-07-20 01:56:11 -0400

    Level calculaation

--- src/main/java/rebelmythik/antivillagerlag/utils/CalculateLevel.java
+++ src/main/java/rebelmythik/antivillagerlag/utils/CalculateLevel.java
@@ -9,8 +9,24 @@ public class CalculateLevel {
         int vilEXP = vil.getVillagerExperience();
         int vilLVL = vil.getVillagerLevel();
 
-        if (vilLVL == 1 && vilEXP < 10) return 1;
+        // Level 2 Calculation
+        if (vilLVL == 1 && vilEXP >= 10 && vilEXP < 70) return 2;
+        if (vilLVL == 1 && vilEXP >= 70 && vilEXP < 150) return 3;
+        if (vilLVL == 1 && vilEXP >= 150 && vilEXP < 250) return 4;
+        if (vilLVL == 1 && vilEXP >= 250) return 5;
 
-        return 5;
+        // Level 3 Calculation
+        if (vilLVL == 2 && vilEXP >= 70) return 3;
+        if (vilLVL == 2 && vilEXP >= 150) return 4;
+        if (vilLVL == 2 && vilEXP >= 250) return 5;
+
+        // Level 4 Calculation
+        if (vilLVL == 3 && vilEXP >= 150) return 4;
+        if (vilLVL == 3 && vilEXP >= 250) return 5;
+
+        // Level 5 Calculation
+        if (vilLVL == 4 && vilEXP >= 250) return 5;
+
+        return 0;
     }
 }
