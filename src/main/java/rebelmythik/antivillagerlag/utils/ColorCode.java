831fafbd7d6acb47f488b5c45cb7fccf7fd5c6ea
RebelMythik <50276986+RebelMythik@users.noreply.github.com>
2022-07-22 01:17:41 -0400

    ColorCode

---
+++ src/main/java/rebelmythik/antivillagerlag/utils/ColorCode.java
@@ -0,0 +1,11 @@
+package rebelmythik.antivillagerlag.utils;
+
+
+public class ColorCode {
+
+    public String cm(String s) {
+
+        return org.bukkit.ChatColor.translateAlternateColorCodes('&', s);
+
+    }
+}
